3f9c485854115f7ddcf702be4b04752bb0e2596e
超神的冰凉 <2801621420@qq.com>
2020-03-13 18:13:58 +0800

    更新

--- js/custom/myblocks.js
+++ js/custom/myblocks.js
@@ -126,7 +126,7 @@ Blockly.JavaScript['bnmanager'] = function(block) {
 Blockly.Blocks['bn_register_command'] = {
   init: function() {
     this.jsonInit({
-      "message0": '注册命令:使用 %1 命令名 %2 描述 %3 回调函数名 %4 ',
+      "message0": '注册命令:使用 %1 命令名 %2 描述 %3 回调函数名(sender,args) %4 ',
       "args0": [
 		{
 		  "type": "input_value",
@@ -173,7 +173,7 @@ Blockly.JavaScript['bn_register_command'] = function(block) {
 Blockly.Blocks['bn_register_delay'] = {
   init: function() {
     this.jsonInit({
-      "message0": '延迟调用:使用 %1 回调函数名 %2  ',
+      "message0": '延迟调用:使用 %1 回调函数名(tick) %2 延迟(刻) %3 ',
       "args0": [
 		{
 		  "type": "input_value",
@@ -182,38 +182,64 @@ Blockly.Blocks['bn_register_delay'] = {
 		},
         {
           "type": "input_value",
-          "name": "CMD",
+          "name": "CALLBACK",
           "check": "String"
         },
 		{
 		  "type": "input_value",
-		  "name": "DESCRIPTION",
-		  "check": "String"
-		},
-		{
-		  "type": "input_value",
-		  "name": "CALLBACK",
-		  "check": "String"
+		  "name": "DELAY",
+		  "check": "Number"
 		}
       ],
 	  "nextStatement": null,
 	  "previousStatement": null,
       "colour": 65,
-      "tooltip": "注册一个命令",
+      "tooltip": "延时调用一个函数",
       "helpUrl": "#"
     });
   }
 };
-Blockly.JavaScript['bn_register_command'] = function(block) {
+Blockly.JavaScript['bn_register_delay'] = function(block) {
   //String or array length.
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'CMD',
-      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-  var argument1 = Blockly.JavaScript.valueToCode(block, 'DESCRIPTION',
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'DELAY',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
   var argument2 = Blockly.JavaScript.valueToCode(block, 'CALLBACK',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
   var managertmp = Blockly.JavaScript.valueToCode(block, 'MANAGER',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-  var text=managertmp+".createCommand("+argument0+","+argument1+","+argument2+");\n"
+  var text=managertmp+".createTask("+argument2+","+argument1+");\n"
+  return text;
+};
+Blockly.Blocks['bn_register_repeat20'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": '后台循环:使用 %1 回调函数名(tick) %2 并每20秒执行一次 ',
+      "args0": [
+		{
+		  "type": "input_value",
+		  "name": "MANAGER",
+		  "check": "BNManager"
+		},
+        {
+          "type": "input_value",
+          "name": "CALLBACK",
+          "check": "String"
+        }
+      ],
+	  "nextStatement": null,
+	  "previousStatement": null,
+      "colour": 65,
+      "tooltip": "每20秒循环调用一个函数",
+      "helpUrl": "#"
+    });
+  }
+};
+Blockly.JavaScript['bn_register_repeat20'] = function(block) {
+  //String or array length.
+  var argument2 = Blockly.JavaScript.valueToCode(block, 'CALLBACK',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var managertmp = Blockly.JavaScript.valueToCode(block, 'MANAGER',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text=managertmp+".createLoopTask("+argument2+",40);\n"
   return text;
 };
