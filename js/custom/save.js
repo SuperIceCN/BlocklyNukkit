90d898e2826bb202c4238aec8a00d146818245b7
13116106601 <charmaznable@vip.qq.com>
2020-03-15 04:04:59 +0800

    更新保存功能

--- js/custom/save.js
+++ js/custom/save.js
@@ -13,7 +13,7 @@ document.onkeydown = function(){
 	var oEvent = window.event;
 	if (oEvent.keyCode == 83 && oEvent.altKey) {
 		    var content = document.getElementById("jspreview").innerText;
-			var name = prompt("请输入您欲保存的文件名\n不需要以.js结尾", "");
+			var name = prompt("请输入您欲保存的插件文件名\n不需要以.js结尾", "");
 			if (name != null) {
 			    var name1 = name+".js"
 				savecode(name1, content);
