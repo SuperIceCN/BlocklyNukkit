e50bc68295aa2e5caeb2c4696a1e46ee9ca32f9a
超神的冰凉 <2801621420@qq.com>
2020-03-14 16:17:36 +0800

    更新聊天消息

--- js/custom/event.js
+++ js/custom/event.js
@@ -61,14 +61,14 @@ Blockly.Blocks['event_getjoinmessage'] = {
 		  "check": "Event"
 		}
       ],
-	  "output": "Player",
+	  "output": "String",
       "colour": 10,
       "tooltip": "获取事件的进服消息(getJoinMessage)",
       "helpUrl": "#"
     });
   }
 };
-Blockly.JavaScript['player_playedbefore'] = function(block) {
+Blockly.JavaScript['event_getjoinmessage'] = function(block) {
   //String or array length.
   var valtmp = Blockly.JavaScript.valueToCode(block, 'VAL',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
@@ -94,7 +94,7 @@ Blockly.Blocks['event_setJoinMessage'] = {
 	  "nextStatement": null,
 	  "previousStatement": null,
       "colour": 10,
-      "tooltip": "取消事件(setJoinMessage)",
+      "tooltip": "设置进服消息(setJoinMessage)",
       "helpUrl": "#"
     });
   }
@@ -106,5 +106,63 @@ Blockly.JavaScript['event_setJoinMessage'] = function(block) {
   var mes = Blockly.JavaScript.valueToCode(block, 'MESSAGE',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
   var text=eventtmp+".setJoinMessage("+mes+");\n"
+  return text;
+};
+Blockly.Blocks['event_getchatformat'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": '获取事件 %1 的聊天消息',
+      "args0": [
+		{
+		  "type": "input_value",
+		  "name": "VAL",
+		  "check": "Event"
+		}
+      ],
+	  "output": "String",
+      "colour": 10,
+      "tooltip": "获取事件的聊天消息(getFormat)",
+      "helpUrl": "#"
+    });
+  }
+};
+Blockly.JavaScript['event_getchatformat'] = function(block) {
+  //String or array length.
+  var valtmp = Blockly.JavaScript.valueToCode(block, 'VAL',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text=valtmp+".getFormat()";
+  return [text, Blockly.JavaScript.ORDER_MEMBER];
+};
+Blockly.Blocks['event_setchatformat'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": '设置事件 %1 的聊天消息为 %2 ',
+      "args0": [
+		{
+		  "type": "input_value",
+		  "name": "EVENT",
+		  "check": "Event"
+		},
+		{
+		  "type": "input_value",
+		  "name": "MESSAGE",
+		  "check": "String"
+		}
+      ],
+	  "nextStatement": null,
+	  "previousStatement": null,
+      "colour": 10,
+      "tooltip": "设置聊天消息(setFormat)",
+      "helpUrl": "#"
+    });
+  }
+};
+Blockly.JavaScript['event_setchatformat'] = function(block) {
+  //String or array length.
+  var eventtmp = Blockly.JavaScript.valueToCode(block, 'EVENT',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var mes = Blockly.JavaScript.valueToCode(block, 'MESSAGE',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text=eventtmp+".setFormat("+mes+");\n"
   return text;
 };
