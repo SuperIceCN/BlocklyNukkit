c06463084680771e38819b36f83c0034a1c14104
超神的冰凉 <2801621420@qq.com>
2020-03-17 17:32:00 +0800

    更新对话框

--- js/custom/window.js
+++ js/custom/window.js
@@ -73,12 +73,12 @@ Blockly.JavaScript['window_buildButton'] = function(block) {
 Blockly.Blocks['window_showToPlayer'] = {
   init: function() {
     this.jsonInit({
-      "message0": '使用 %1 给玩家 %2 发送简单窗口并回调 %3 函数',
+      "message0": '使用 %1 给玩家 %2 发送窗口并回调 %3 函数',
       "args0": [
 		{
 		  "type": "input_value",
 		  "name": "BUILDER",
-		  "check": "SimpleWindowBuilder"
+		  "check": ["SimpleWindowBuilder","ModalWindowBuilder"]
 		},
 		{
 		  "type": "input_value",
@@ -94,7 +94,7 @@ Blockly.Blocks['window_showToPlayer'] = {
 	  "nextStatement": null,
 	  "previousStatement": null,
       "colour": 240,
-      "tooltip": "展示简单窗口(showToPlayer)",
+      "tooltip": "展示窗口(showToPlayer)",
       "helpUrl": "#"
     });
   }
@@ -108,5 +108,76 @@ Blockly.JavaScript['window_showToPlayer'] = function(block) {
   var callbacktmp = Blockly.JavaScript.valueToCode(block, 'CALLBACK',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
   var text=buildertmp+".showToPlayer("+playertmp+","+callbacktmp+");\n"
+  return text;
+};
+Blockly.Blocks['window_getModalWindowBuilder'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": '构造一个标题为 %1 内容为 %2 的对话框管理器',
+      "args0": [
+		{
+		  "type": "input_value",
+		  "name": "TITLE",
+		  "check": "String"
+		},
+		{
+		  "type": "input_value",
+		  "name": "CONTEXT",
+		  "check": "String"
+		}
+      ],
+	  "output": "SimpleWindowBuilder",
+      "colour": 240,
+      "tooltip": "构造对话框管理器(getModalWindowBuilder)",
+      "helpUrl": "#"
+    });
+  }
+};
+Blockly.JavaScript['window_getModalWindowBuilder'] = function(block) {
+  var titletmp = Blockly.JavaScript.valueToCode(block, 'TITLE',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var contexttmp = Blockly.JavaScript.valueToCode(block, 'CONTEXT',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text='window.getModalWindowBuilder('+titletmp+','+contexttmp+')';
+  return [text, Blockly.JavaScript.ORDER_MEMBER];
+};
+Blockly.Blocks['window_setModalButtons'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": '设置对话框 %1 第一个按钮文本 %2 第二个按钮文本 %3 ',
+      "args0": [
+		{
+		  "type": "input_value",
+		  "name": "BUILDER",
+		  "check": "SimpleWindowBuilder"
+		},
+		{
+		  "type": "input_value",
+		  "name": "TEXT1",
+		  "check": "String"
+		},
+		{
+		  "type": "input_value",
+		  "name": "TEXT2",
+		  "check": "String"
+		}
+      ],
+	  "nextStatement": null,
+	  "previousStatement": null,
+      "colour": 240,
+      "tooltip": "设置按钮(setButton1&setButton2)",
+      "helpUrl": "#"
+    });
+  }
+};
+Blockly.JavaScript['window_setModalButtons'] = function(block) {
+  //String or array length.
+  var buildertmp = Blockly.JavaScript.valueToCode(block, 'BUILDER',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text1tmp = Blockly.JavaScript.valueToCode(block, 'TEXT1',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text2tmp = Blockly.JavaScript.valueToCode(block, 'TEXT2',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var text=buildertmp+".setButton1("+text1tmp+");\n"+buildertmp+".setButton2("+text2tmp+");\n"
   return text;
 };
